8b2fac5e1680581d975a58cbd6e95744e4bc891f
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-11 13:46:28 +0100

    adding sensible naming

--- resources/com/stushq/scripts/linux/runFlowProc.sh
+++ resources/com/stushq/scripts/linux/runFlowProc.sh
@@ -3,7 +3,7 @@ echo "running flow"
 CREDS=$1
 FLOWSERVER=$2
 jobid=`curl -D- -u ${CREDS} \
---insecure -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
+--insecure -vvv -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
 -H "accept: application/json" \
--d '{"actualParameter":[{"actualParameterName":"arg1","value":"123"}]}'`
+-d '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}'`
 echo $jobid | grep jobId |cut -d '"' -f 4
