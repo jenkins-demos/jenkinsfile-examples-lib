29433b007a13312403bc2128febb68c0cc092eff
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-11 13:43:54 +0100

    adding sensible naming

--- resources/com/stushq/scripts/linux/runFlowProc.sh
+++ resources/com/stushq/scripts/linux/runFlowProc.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 echo "running flow"
+CREDS=$1
+FLOWSERVER=$2
+jobid=`curl -D- -u ${CREDS} \
+--insecure -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
+-H "accept: application/json" \
+-d '{"actualParameter":[{"actualParameterName":"arg1","value":"123"}]}'`
+echo $jobid | grep jobId |cut -d '"' -f 4
--- vars/runFlowProc.groovy
+++ vars/runFlowProc.groovy
@@ -1,4 +1,4 @@
 def call(Map config) {
     loadLinuxScript(name: 'runFlowProc.sh')
-    sh "./runFlowProc.sh"
+    sh "./runFlowProc.sh sbrown:sbrown https://ps9.ecloud-kdemo.com"
 }
