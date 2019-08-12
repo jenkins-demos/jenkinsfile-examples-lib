2b436c9eb1af5f0eb8c908c961fbbc61d5f79097
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-12 18:06:47 +0100

    adding in srtyuff

--- resources/com/stushq/scripts/linux/runFlowProc.sh
+++ resources/com/stushq/scripts/linux/runFlowProc.sh
@@ -3,20 +3,20 @@ echo "running flow"
 CREDS=$1
 FLOWSERVER=$2
 ret=`curl -D- -u ${CREDS} \
---insecure  -vvv -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
+--insecure  -v -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
 -H "accept: application/json" \
 -d '{"actualParameter":[{"actualParameterName":"arg2","value":"1234567"}]}'`
-#echo $ret | grep jobId |cut -d '"' -f 4
-build_id=$(echo $ret | grep jobId |cut -d '"' -f 4)
-echo "$build_id"
-# af300919-bd1e-11e9-ae46-02425352dfc1
+
+job_id=$(echo $ret | grep jobId |cut -d '"' -f 4)
+#echo "$job_id"
+
 
 pat="[a-z]|[A-Z]|[\-][0-9]"
-
-if [[ ! "${build_id}" =~ $pat ]];
+if [[ ! "${job_id}" =~ $pat ]];
 then
-  echo "failed call"
+  echo "Failed calling flow proceedure, no job id available "
   exit 1
 else
+  echo "Job id = ${job_id}"
   exit 0
 fi
