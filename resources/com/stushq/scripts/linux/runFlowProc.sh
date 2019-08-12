7972e2377c37b278d6dd75686624b6fe9e1c708e
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-12 18:03:58 +0100

    adding in srtyuff

--- resources/com/stushq/scripts/linux/runFlowProc.sh
+++ resources/com/stushq/scripts/linux/runFlowProc.sh
@@ -5,7 +5,7 @@ FLOWSERVER=$2
 ret=`curl -D- -u ${CREDS} \
 --insecure  -vvv -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
 -H "accept: application/json" \
--d '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}]}'`
+-d '{"actualParameter":[{"actualParameterName":"arg2","value":"1234567"}]}'`
 #echo $ret | grep jobId |cut -d '"' -f 4
 build_id=$(echo $ret | grep jobId |cut -d '"' -f 4)
 echo "$build_id"
