f913803f0ec93d7935c73ebcb434147726d4d138
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-12 17:09:40 +0100

    adding in srtyuff

--- vars/runFlowProc.groovy
+++ vars/runFlowProc.groovy
@@ -1,6 +1,7 @@
 def call(Map config) {
     loadLinuxScript(name: 'runFlowProc.sh')
-    def r = sh "./runFlowProc.sh ${config.flowCreds} ${config.flowServer}"
-    echo "ret is $r"
-    
+    def proc ='./runFlowProc.sh ${config.flowCreds} ${config.flowServer}'.execute()
+    //def r = sh "./runFlowProc.sh ${config.flowCreds} ${config.flowServer}"
+    //echo "ret is $r"
+
 }
