2f74a25cc0f0378ff7ec485fd01b1d06141637ea
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-12 20:00:51 +0100

    adding in srtyuff

--- vars/runFlowProc.groovy
+++ vars/runFlowProc.groovy
@@ -1,9 +1,9 @@
 def call(Map config) {
     loadLinuxScript(name: 'runFlowProc.sh')
 
-    def json_str = '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}'
-    echo "found jason string  $json_str"
-    def r = sh "./runFlowProc.sh ${config.flowCreds} ${config.flowServer} ${json_str}"
+    //def json_str = '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}'
+    //echo "found jason string  $json_str"
+    def r = sh "./runFlowProc.sh ${config.flowCreds} ${config.flowServer}"
     echo "ret is $r"
 
 }
