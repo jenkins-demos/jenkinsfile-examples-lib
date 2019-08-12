4af55177d063c9091b0c0af1a2314214afae014b
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-12 18:40:30 +0100

    adding in srtyuff

--- resources/com/stushq/scripts/linux/runFlowProc.sh
+++ resources/com/stushq/scripts/linux/runFlowProc.sh
@@ -10,7 +10,7 @@ echo "received [${DAT}]"
 
 
 ret=`curl -D- -u ${CREDS} \
---insecure  -v -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
+--insecure  -vvvv -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
 -H "accept: application/json" \
 -d '$DAT'`
 
