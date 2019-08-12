52510bb1cb507b0da7522ae1f8f8fbf92d67114b
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-12 18:59:03 +0100

    adding in srtyuff

--- resources/com/stushq/scripts/linux/runFlowProc.sh
+++ resources/com/stushq/scripts/linux/runFlowProc.sh
@@ -8,7 +8,7 @@ echo "received [${DAT}]"
 
 
 fg=${generate_post_data}
-echo "fg is $fg"
+echo "fg is ${fg}"
 
 ret=`curl -D- -u ${CREDS} \
 --insecure  -vvvv -X POST "${FLOWSERVER}/rest/v1.0/jobs?request=runProcedure&projectName=Default&procedureName=Echo" \
