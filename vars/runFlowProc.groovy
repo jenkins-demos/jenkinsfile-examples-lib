73596c2f5fcf1322071f50f921c512fe43d34091
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-14 11:20:27 +0100

    moving json args

--- vars/runFlowProc.groovy
+++ vars/runFlowProc.groovy
@@ -1,8 +1,5 @@
 def call(Map config) {
     loadLinuxScript(name: 'runFlowProc.sh')
-    echo "curl options are in script ${config.flowCreds} ${config.flowServer} ${config.jsonArgs} ${config.procName} ${config.projName} ${config.curlOptions}"
-    //def json_str = '{"actualParameter":[{"actualParameterName":"arg1","value":"1234567"}'
-    //echo "found jason string  $json_str"
     def r = sh "./runFlowProc.sh ${config.flowCreds} ${config.flowServer} ${config.jsonArgs} ${config.procName} ${config.projName} ${config.curlOptions}"
     echo "ret is $r"
 
