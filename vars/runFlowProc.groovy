1fd0207e77e002ae210067ea4111164e455b2b5f
stubrowncloudbees <sbrown@cloudbees.com>
2019-08-14 12:31:13 +0100

    moving json args

--- vars/runFlowProc.groovy
+++ vars/runFlowProc.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
     loadLinuxScript(name: 'runFlowProc.sh')
     def r = sh label: 'runFlowProc.sh', returnStdout: true, script: "./runFlowProc.sh ${config.flowCreds} ${config.flowServer} ${config.jsonArgs} ${config.procName} ${config.projName} ${config.curlOptions}"
-    //return r
+    return r
 
 
 }
