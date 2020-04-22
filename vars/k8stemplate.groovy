36bd045edb092f2903cc2c1c6e526bbe1cd0ef5d
stubrowncloudbees <33249923+stubrowncloudbees@users.noreply.github.com>
2020-04-22 08:10:04 +0100

    Update k8stemplate.groovy

--- vars/k8stemplate.groovy
+++ vars/k8stemplate.groovy
@@ -1,1 +1,32 @@
-corporatePipeline.groovy
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+
+    agent {
+        kubernetes {
+            label 'my-pod-template'
+            defaultContainer 'jnlp'
+            yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: some-label-value
+spec:
+  containers:
+  - name: maven
+    image: maven:alpine
+    command:
+    - cat
+    tty: true
+  - name: busybox
+    image: busybox
+    command:
+    - cat
+    tty: true
+"""
+        }
+    }
