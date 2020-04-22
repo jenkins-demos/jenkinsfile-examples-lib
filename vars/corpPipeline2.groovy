d46319ed9694ddd8a4bb20fbfa726b6dd6b2a986
stubrowncloudbees <33249923+stubrowncloudbees@users.noreply.github.com>
2020-04-22 08:16:46 +0100

    Update corpPipeline2.groovy

--- vars/corpPipeline2.groovy
+++ vars/corpPipeline2.groovy
@@ -5,8 +5,31 @@ def call(body) {
     body()
 
     pipeline {
-        k8stemplate{
+        agent {
+        kubernetes {
+            label 'my-pod-template'
+            defaultContainer 'jnlp'
+            yaml """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: some-label-value
+spec:
+  containers:
+  - name: maven
+    image: maven:alpine
+    command:
+    - cat
+    tty: true
+  - name: busybox
+    image: busybox
+    command:
+    - cat
+    tty: true
+"""
         }
+    
         stages {
             stage('Run maven') {
                 steps {
